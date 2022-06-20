399fe45531024fc7dbafccfed63efad819b6c4d1
Aarya Chopkar <aaryachopkar@gmail.com>
2022-06-20 17:05:55 +0530

    Hero

---
+++ src/components/Hero.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import hero from '../images/photo-grid.png'
+
+function Hero(){
+    return(
+        <>
+        <section className="hero">
+            <img src={hero} alt="hero" className="photo" />
+            <h1>Online Experiences</h1>
+            <p>Join unique interactive activities led by
+                <br></br>
+             one-of-a-kind hosts—all without leaving home.</p>
+        </section>
+        </>
+    )
+}
+
+export default Hero;
