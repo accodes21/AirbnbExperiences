91318dc86ef33977fb48ae156b0279464ed70a81
Aarya Chopkar <aaryachopkar@gmail.com>
2022-06-28 09:46:33 +0530

    Cards Done

--- src/App.js
+++ src/App.js
@@ -2,13 +2,32 @@ import './App.css';
 import Card from './components/Card';
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
+import data from './components/data'
 
 function App() {
+  const cards = data.map(item => {
+    return (
+      <Card
+      key = {item.id}
+      item = {item}
+
+      //LONGER METHOD:
+      // img={item.coverImg}
+      // rating={item.stats.rating}
+      // reviewCount={item.stats.reviewCount}
+      // country={item.location}
+      // title={item.title}
+      // price={item.price}
+      />
+    )
+  })
   return (
    <>
    <Navbar/>
    <Hero/>
-   <Card/>
+   <section className='cards-list'>
+   {cards}
+   </section>
    </>
   );
 }
